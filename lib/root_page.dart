e1b267ccc2e45afdd66f3b52cbb871e020a31bb1
eomchanu <djacksdn1@icloud.com>
2023-11-19 16:09:34 +0900

    :art: Fix root_page.dart
    Add fixed appBar

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -2,6 +2,7 @@ import 'package:fam_story_frontend/pages/calendar_page.dart';
 import 'package:fam_story_frontend/pages/home_page.dart';
 import 'package:fam_story_frontend/pages/post_page.dart';
 import 'package:flutter/material.dart';
+import 'style.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -13,18 +14,32 @@ class RootPage extends StatefulWidget {
 class _RootPageState extends State<RootPage> {
   int _selectedIndex = 0;
 
-  final List<Widget> _pages = [const HomePage(), const PostPage(), const CalendarPage()];
+  final List<Widget> _pages = [
+    const HomePage(),
+    const PostPage(),
+    const CalendarPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(40.0),
+        child: AppBar(
+          backgroundColor: AppColor.backgroudColor,
+        ),
+      ),
+      backgroundColor: AppColor.backgroudColor,
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.chat_bubble_outline_rounded), label: 'Post'),
-          BottomNavigationBarItem(icon: Icon(Icons.calendar_today_rounded), label: 'Calendar'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble_outline_rounded), label: 'Post'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today_rounded), label: 'Calendar'),
         ],
         onTap: (index) {
           setState(() {
