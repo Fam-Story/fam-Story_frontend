5e0c8f741dbf542bcb31af3bcce5e9482a5c5115
frankwon11 <frankwon11@gmail.com>
2023-11-20 23:07:33 +0900

    :package: Add: flutter_secure_storage package 연동 (#6)
    로그인 토큰 저장 시 필요

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,7 +8,7 @@ class ApiService {
   static const String baseUrl = 'https://famstory.thisiswandol.com';
 
   // /user 유저 생성
-  static Future<bool> createUser(String email, String username, String password, String nickname, int age, int gender) async {
+  static Future<bool> createUser(String email, String username, String password, String? nickname, int age, int gender) async {
     final url = Uri.parse('$baseUrl/user');
 
     final response = await http.post(
@@ -23,5 +23,27 @@ class ApiService {
       return true;
     }
     throw ErrorDescription('Something wrong to create User ID');
+  }
+
+  // /user/login 유저 로그인
+  static Future<bool> postUserLogin(String email, String username, String password, String nickname, int age, int gender) async {
+    final url = Uri.parse('$baseUrl/user/login');
+
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({"email": email, "password": password}),
+    );
+
+    if (response.statusCode == 201) {
+      final Map<String, dynamic> data = jsonDecode(response.body)['data'];
+      final String token = data['token'];
+      bool isBelongedToFamily = data['isBelongedToFamily'];
+
+      return isBelongedToFamily;
+    }
+    throw ErrorDescription('Something wrong to login');
   }
 }
