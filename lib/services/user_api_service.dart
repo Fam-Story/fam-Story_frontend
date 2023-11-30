4f1d9c6eda1884db90f84172de4a06cd6227367f
eomchanu <djacksdn1@icloud.com>
2023-11-30 17:46:17 +0900

    :recycle: Refactor: implement SignUpCompleteDialog and SignUpCompleteDialog

--- lib/services/user_api_service.dart
+++ lib/services/user_api_service.dart
@@ -9,7 +9,8 @@ class UserApiService {
   static const String baseUrl = 'https://famstory.thisiswandol.com/api';
 
   /// POST: /user [회원가입] 회원 가입
-  static Future<bool> postUser(String email, String username, String password, String nickname, int age, int gender) async {
+  static Future<bool> postUser(String email, String username, String password,
+      String nickname, int age, int gender) async {
     final url = Uri.parse('$baseUrl/user');
 
     final response = await http.post(
@@ -17,27 +18,42 @@ class UserApiService {
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
-      body: jsonEncode({"email": email, "username": username, "password": password, "nickname": nickname, "age": age, "gender": gender}),
+      body: jsonEncode({
+        "email": email,
+        "username": username,
+        "password": password,
+        "nickname": nickname,
+        "age": age,
+        "gender": gender
+      }),
     );
 
+    String errorMessage = 'ERROR';
     if (response.statusCode == 201) {
       return true;
+    } else if (response.statusCode == 409) {
+      errorMessage = 'Email address already exists.';
     }
-    // 예외 처리; 메시지를 포함한 예외를 던짐
-    String errorMessage = jsonDecode(response.body)['message'] ?? 'Error';
     print(errorMessage);
     throw ErrorDescription(errorMessage);
   }
 
   /// PUT: /user [프로필] 회원 정보 수정
-  static Future<bool> putUser({String? email, String? username, String? password, String? nickname, int? age, int? gender}) async {
+  static Future<bool> putUser(
+      {String? email,
+      String? username,
+      String? password,
+      String? nickname,
+      int? age,
+      int? gender}) async {
     final url = Uri.parse('$baseUrl/user');
     const storage = FlutterSecureStorage();
 
     String? userInfoString = await storage.read(key: 'login');
 
     if (userInfoString == null) {
-      throw ErrorDescription('Your login token has expired. Please Login Again.');
+      throw ErrorDescription(
+          'Your login token has expired. Please Login Again.');
     }
     Map<String, dynamic> userInfo = json.decode(userInfoString);
     String loginToken = userInfo['token'];
@@ -83,14 +99,16 @@ class UserApiService {
   }
 
   /// DELETE: /user [프로필] 회원 탈퇴
-  static Future<bool> deleteUser(String email, String username, String password, String? nickname, int age, int gender) async {
+  static Future<bool> deleteUser(String email, String username, String password,
+      String? nickname, int age, int gender) async {
     final url = Uri.parse('$baseUrl/user');
     const storage = FlutterSecureStorage();
 
     String? userInfoString = await storage.read(key: 'login');
 
     if (userInfoString == null) {
-      throw ErrorDescription('Your login token has expired. Please Login Again.');
+      throw ErrorDescription(
+          'Your login token has expired. Please Login Again.');
     }
     Map<String, dynamic> userInfo = json.decode(userInfoString);
     String loginToken = userInfo['token'];
@@ -114,14 +132,16 @@ class UserApiService {
   }
 
   /// GET: /user [프로필] 회원 정보 조회
-  static Future<UserModel> getUser(String email, String username, String password, String? nickname, int age, int gender) async {
+  static Future<UserModel> getUser(String email, String username,
+      String password, String? nickname, int age, int gender) async {
     final url = Uri.parse('$baseUrl/user');
     const storage = FlutterSecureStorage();
 
     String? userInfoString = await storage.read(key: 'login');
 
     if (userInfoString == null) {
-      throw ErrorDescription('Your login token has expired. Please Login Again.');
+      throw ErrorDescription(
+          'Your login token has expired. Please Login Again.');
     }
     Map<String, dynamic> userInfo = json.decode(userInfoString);
     String loginToken = userInfo['token'];
@@ -146,7 +166,8 @@ class UserApiService {
   }
 
   /// POST: /user/login [로그인] 로그인 후 JWT 토큰 발급
-  static Future<int> postUserLogin(String email, String password, bool autoLogin) async {
+  static Future<int> postUserLogin(
+      String email, String password, bool autoLogin) async {
     final url = Uri.parse('$baseUrl/user/login');
     const storage = FlutterSecureStorage();
 
@@ -171,7 +192,9 @@ class UserApiService {
       }
 
       // 시스템 저장소에 토큰 저장; 자동로그인인 경우 email과 password도 저장
-      var loginValue = autoLogin ? jsonEncode({"token": token, "email": email, "password": password}) : jsonEncode({"token": token});
+      var loginValue = autoLogin
+          ? jsonEncode({"token": token, "email": email, "password": password})
+          : jsonEncode({"token": token});
       await storage.write(key: 'login', value: loginValue);
 
       print(token);
