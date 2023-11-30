65ceae77bbe91cfea4ff9aa74cdfcb9bfe3123a3
frankwon11 <frankwon11@gmail.com>
2023-11-30 11:42:14 +0900

    :sparkles: Feat: 가족 멤버 수정 삭제 기능 추가 (#38)

--- lib/services/family_member_api_service.dart
+++ lib/services/family_member_api_service.dart
@@ -11,7 +11,7 @@ class FamilyMemberApiService {
   // TODO: FCM 토큰 생성 필요
   static const String tempFcmToken = 'seungwonTest';
 
-  // POST: 가족 생성
+  /// POST: /family-member [가족 구성원] 가족 구성원 생성
   static Future<int> postFamilyMember(int familyId, int role) async {
     final url = Uri.parse('$baseUrl/family-member');
     const storage = FlutterSecureStorage();
@@ -41,5 +41,74 @@ class FamilyMemberApiService {
       return familyMemberId;
     }
     throw ErrorDescription('Something Wrong To Join Family');
+  }
+
+  /// PUT: /family-member [가족 구성원] 가족 구성원 역할 수정
+  static Future<bool> putFamilyMember(int familyMemberId, int role) async {
+    final url = Uri.parse('$baseUrl/family-member');
+    const storage = FlutterSecureStorage();
+
+    String? userInfoString = await storage.read(key: 'login');
+
+    if (userInfoString == null) {
+      throw ErrorDescription('Your login token has expired. Please Login Again.');
+    }
+    Map<String, dynamic> userInfo = json.decode(userInfoString);
+    String loginToken = userInfo['token'];
+
+    // TODO: 가족 멤버 정보 수정 창에서 null 체크 하기
+    // 가족 이름만 수정 가능
+    Map<String, dynamic> data = {};
+    data.putIfAbsent("familyMemberId", () => familyMemberId);
+    data.putIfAbsent("role", () => role);
+
+    final response = await http.put(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $loginToken',
+      },
+      body: jsonEncode(data),
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+    // 예외 처리; 메시지를 포함한 예외를 던짐
+    String errorMessage = jsonDecode(response.body)['message'] ?? 'Error';
+    print(errorMessage);
+    throw ErrorDescription(errorMessage);
+  }
+
+  /// DELETE: /family-member [가족 구성원] 가족 구성원 삭제
+  static Future<bool> deleteUser(int familyMemberId) async {
+    final url = Uri.parse('$baseUrl/family-member?family-memberId=$familyMemberId');
+    const storage = FlutterSecureStorage();
+
+    String? userInfoString = await storage.read(key: 'login');
+
+    if (userInfoString == null) {
+      throw ErrorDescription('Your login token has expired. Please Login Again.');
+    }
+    Map<String, dynamic> userInfo = json.decode(userInfoString);
+    String loginToken = userInfo['token'];
+
+    final response = await http.delete(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $loginToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+    // 예외 처리; 메시지를 포함한 예외를 던짐
+    String errorMessage = jsonDecode(response.body)['message'] ?? 'Error';
+    print(errorMessage);
+    throw ErrorDescription(errorMessage);
   }
 }
