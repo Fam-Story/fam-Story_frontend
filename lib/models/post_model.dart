0a5b5caf093e21a6158c0f594f858ef64edfc460
eomchanu <djacksdn1@icloud.com>
2023-12-04 15:36:07 +0900

    :sparkles: Feat: implement post api services

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -1,10 +1,22 @@
 class PostModel {
-  final int postId, familyMemberId;
-  final String context, createdDate;
+  final int postId, familyMemberId, familyId;
+  final String context,
+      title,
+      createdYear,
+      createdMonth,
+      createdDay,
+      createdHour,
+      createdMinute;
 
   PostModel.fromJson(Map<String, dynamic> json)
       : postId = json['postId'],
         familyMemberId = json['familyMemberId'],
+        title = json['title'],
         context = json['context'],
-        createdDate = json['createdDate'];
+        createdYear = json['createdYear'],
+        createdMonth = json['createdMonth'],
+        createdDay = json['createdDay'],
+        createdHour = json['createdHour'],
+        createdMinute = json['createdMinute'],
+        familyId = json['familyId'];
 }
