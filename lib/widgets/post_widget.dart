24c95fd349cff100c2539891934e99de598cffd1
eomchanu <djacksdn1@icloud.com>
2023-12-05 12:42:21 +0900

    :sparkles: Feat: connecting post api service complete (#40)

--- lib/widgets/post_widget.dart
+++ lib/widgets/post_widget.dart
@@ -1,6 +1,7 @@
 import 'package:fam_story_frontend/models/post_model.dart';
 import 'package:fam_story_frontend/services/post_api_service.dart';
 import 'package:fam_story_frontend/style.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PostWidget extends StatefulWidget {
@@ -23,6 +24,7 @@ class _PostWidgetState extends State<PostWidget> {
 
   @override
   Widget build(BuildContext context) {
+    print(post.context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
       child: SizedBox(
@@ -33,45 +35,32 @@ class _PostWidgetState extends State<PostWidget> {
             Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                GestureDetector(
-                  onLongPressEnd: (details) {
-                    // TODO: delete
-                    PostApiService.deletePost(post.postId);
-                  },
-                  onTap: () {
-                    _showPostDialog(context);
-                  },
-                  child: Container(
-                    width: 165,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      // TODO: role별 색상으로 변경
-                      color: AppColor.objectColor,
-                      borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(20)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 20,
-                          spreadRadius: 5,
-                        ),
-                      ],
-                    ),
+                Container(
+                  width: 155,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    // TODO: role별 색상으로 변경
+                    color: AppColor.objectColor,
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 20,
+                        spreadRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Center(
                     child: Padding(
                       padding: const EdgeInsets.all(13.0),
-                      child: ListView(shrinkWrap: true, children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                                // overflow: TextOverflow.ellipsis,
-                                post.context,
-                                style: const TextStyle(
-                                    color: AppColor.swatchColor, fontSize: 20))
-                          ],
-                        ),
-                      ]),
+                      child: Center(
+                        child: Text(
+                            // overflow: TextOverflow.ellipsis,
+                            post.context,
+                            style: const TextStyle(
+                                color: AppColor.swatchColor, fontSize: 20)),
+                      ),
                     ),
                   ),
                 ),
@@ -87,51 +76,6 @@ class _PostWidgetState extends State<PostWidget> {
           ],
         ),
       ),
-    );
-  }
-
-  Future<dynamic> _showPostDialog(BuildContext context) {
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return Dialog(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-          child: Container(
-            width: 350,
-            height: 300,
-            decoration: BoxDecoration(
-              // TODO: role별 색상으로 변경
-              color: AppColor.objectColor,
-              borderRadius:
-                  const BorderRadius.only(bottomLeft: Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 20,
-                  spreadRadius: 5,
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: ListView(shrinkWrap: true, children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Text(post.context,
-                          style: const TextStyle(
-                              color: AppColor.swatchColor, fontSize: 20)),
-                    )
-                  ],
-                ),
-              ]),
-            ),
-          ),
-        );
-      },
     );
   }
 }
