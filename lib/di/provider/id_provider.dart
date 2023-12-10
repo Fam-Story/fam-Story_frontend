835f05e5a4d53fab78265a3e6a21274b0fbedfb5
Nam Jin <jinoj@naver.com>
2023-12-11 00:05:21 +0900

    :sparkles: Feat : Provider role, name, nickname, introMessage 추가
    feat : Provider role, name, nickname, introMessage 추가

--- lib/di/provider/id_provider.dart
+++ lib/di/provider/id_provider.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class IdProvider with ChangeNotifier {
   int familyId = 0;
   int familyMemberId = 0;
+  int role = 0;
+  String name = '';
+  String nickname = '';
+  String introMessage = '';
 
   void setFamilyId(int id) {
     familyId = id;
@@ -11,6 +15,26 @@ class IdProvider with ChangeNotifier {
 
   void setFamilyMemberId(int id) {
     familyMemberId = id;
+    notifyListeners();
+  }
+
+  void setRole(int role) {
+    role = role;
+    notifyListeners();
+  }
+
+  void setName(String name) {
+    name = name;
+    notifyListeners();
+  }
+
+  void setNicKName(String nickname) {
+    nickname = nickname;
+    notifyListeners();
+  }
+
+  void setIntroMessage(String introMessage) {
+    introMessage = introMessage;
     notifyListeners();
   }
 }
